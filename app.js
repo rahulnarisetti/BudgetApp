1da77a42dedb1c28b06d2afc6ffb35f7eea98da3
Rahul Narisetti <rahul.narisetti@gmail.com>
2019-06-20 02:55:58 +0530

    Commit-2: Javascript: Table Fields display

--- app.js
+++ app.js
@@ -1,20 +1,82 @@
 
-//BUDGET CONTROLLER 
+//BUDGET CONTROLLER -------------------------------------------------------
 var budgetController=(function()
 {
-    
+    var expense=function(id,des,value)
+    {
+        this.id=id;
+        this.des=des;
+        this.value=value;
+    };
+
+    var income=function(id,des,value)
+    {
+        this.id=id;
+        this.des=des;
+        this.value=value;
+    };
+
+    var data={
+        allitems: {
+            exp: [],
+            inc: []
+        },
+
+        totalitems: {
+            exp: 0,
+            inc: 0
+        }
+
+    };
+
+    return {
+        addNewItem: function(type, des, val)
+        {   var newItem, ID;
+            
+            if(data.allitems[type].length===0)
+            ID=0;
+            else
+            ID=data.allitems[type][data.allitems[type].length-1].id +1;
+            
+            if(type==='exp')
+            {
+                newItem=new expense(ID,des,val);
+            }
+
+            else if(type==='inc')
+            {
+                newItem=new income(ID,des,val);
+            }
+
+            data.allitems[type].push(newItem);
+            return newItem;
+        },
+        testing: function()
+        {
+            console.log(data);
+        }
+    };
+
+
 
 })();
 
 
-//UI CONTROLLER
+
+
+//UI CONTROLLER--------------------------------------------------------
+
+
 var UIController=(function()
 {
     DOMStrings={
         inputType:'.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        clickButton: '.add__btn'
+        clickButton: '.add__btn',
+        expenseContainer: '.expenses__list',
+        incomeContainer: '.income__list'
+                       
     };
 
 
@@ -29,12 +91,38 @@ var UIController=(function()
         },
         getDom: function(){
             return DOMStrings;
+        },
+
+        addListItem: function(obj,type)
+        {
+            var html,newhtml,element;
+            if (type==='inc')
+            {
+                element=DOMStrings.incomeContainer;
+                html='<div class="item clearfix" id="income-%id%"><div class="item__description">%des%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+            }
+
+            else if (type==='exp')
+            {   element=DOMStrings.expenseContainer;
+                html='<div class="item clearfix" id="expense-%id%"><div class="item__description">%des%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            }
+
+            newhtml=html.replace('%id%',obj.id);
+            newhtml=newhtml.replace('%des%',obj.des);
+            newhtml=newhtml.replace('%value%',obj.value);
+
+            document.querySelector(element).insertAdjacentHTML('beforeend',newhtml);
+
         }
     };
 
 })();
 
-// GLOBAL APP CONTROLLER
+
+
+// GLOBAL APP CONTROLLER-------------------------------------------------------------------
+
+
 var Controller=(function(BudgetCon,UICon)
 {   
     var setupEvent=function()
@@ -52,12 +140,21 @@ var Controller=(function(BudgetCon,UICon)
             });
 
     };
+
+
+
     var enterInput=function()
     {
         var inputData=UICon.getInputData();
 
-        console.log(inputData);
-    }
+        var display=BudgetCon.addNewItem(inputData.type,inputData.inputText,inputData.inputNumber);
+
+        UICon.addListItem(display,inputData.type);
+
+    };
+
+
+    
       
     return {
         init: function()
@@ -71,6 +168,8 @@ var Controller=(function(BudgetCon,UICon)
 })(budgetController,UIController);
 
 
+
+//General -----------------------------------------------------------------------
 Controller.init();
 
 
